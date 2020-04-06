58b3f3830c46f5f44cdadea8e679926387d53edf
bill0930 <bill0930@gmail.com>
2020-04-06 23:21:23 +0800

    vue-cli3 basic setup

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+
+
+module.exports = {
+    chainWebpack: config => {
+      config.module
+        .rule('vue')
+        .use('vue-loader')
+        .loader('vue-loader')
+        .tap(options => {
+            options.hotReload = true
+            return options
+          }
+        )},
+    devServer: {
+      hot: false,
+      https: true,
+      liveReload: true,
+      watchContentBase: true
+  //devServer.hot option must be disabled or devServer.watchContentBase option must be enabled in order for liveReload to take effect
+      // https: true,
+    }
+}
