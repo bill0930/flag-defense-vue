d761c135b0f0443f8f5123ad9937b8abc2acfb73
bill0930 <bill0930@gmail.com>
2020-04-11 20:35:24 +0800

    config the vue-toasted plugin

---
+++ src/plugins/vue-toasted.js
@@ -0,0 +1,42 @@
+
+import Vue from 'vue'
+import Toasted from 'vue-toasted';
+import 'material-design-icons/iconfont/material-icons.css';
+
+Vue.use(Toasted,{
+    iconPack : 'material' // set your iconPack, defaults to material. material|fontawesome|custom-class
+  })
+
+// options to the toast
+let options = {
+    type : 'success',
+    icon : {
+      name: 'check'
+    },
+    position: "bottom-center", 
+    theme: 'toasted-primary' ,
+    duration : 1500,
+    fitToScreen: true
+  };
+  
+  // register the toast with the custom message
+  Vue.toasted.register('makeDecision',
+    (payload) => {
+        if (payload.action == 'build'){
+            return `
+            <h5> <span class="text-primary">${payload.winner.name}</span>  
+            has just built  
+            <span class="text-warning">${payload.item}</span> </h5>
+            `;
+        }
+        else if (payload.action == 'destroy'){
+            return `
+            <h5> <span class="text-primary">${payload.winner.name}</span>  
+            has just destroyed 
+            <span class="text-warning">${payload.item}</span> of <span class="text-danger">${payload.loser.name}</span></h5>
+            `
+        }
+        // if there is a message show it with the message
+    },
+    options
+  )
